835203b237775002921bcd25c1be54e60ce694de
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-08-24 18:48:06 -0500

    default view layout for customer completed

--- TrashCollection/wwwroot/js/site.js
+++ TrashCollection/wwwroot/js/site.js
@@ -3,13 +3,3 @@
 
 // Write your JavaScript code.
 
-function InitializeMap() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-        zoom: 8,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), myOptions);
-}
-window.onload = InitializeMap;
